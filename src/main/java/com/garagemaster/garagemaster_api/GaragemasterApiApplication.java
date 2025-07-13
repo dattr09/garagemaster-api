eb68e021c89f43b01a7479d0347e08af76940905
dattr09 <datsml0@gmail.com>
2025-07-13 09:03:18 +0700

    chore: keep empty folders using .gitkeep

---
+++ src/main/java/com/garagemaster/garagemaster_api/GaragemasterApiApplication.java
@@ -0,0 +1,13 @@
+package com.garagemaster.garagemaster_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragemasterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragemasterApiApplication.class, args);
+	}
+
+}
